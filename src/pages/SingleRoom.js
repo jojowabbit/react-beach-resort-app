c9266d19b34a47070409040ed6911a123c0acadb
jojowabbit <jojowabbit@gmail.com>
2020-08-27 15:57:46 +0800

    SingleRoom component setup

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { RoomContext } from "../Context";
 
 export default class SingleRoom extends Component {
+  // to access props by react router dom
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
+  }
+  static contextType = RoomContext;
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
     return <>SingleRoom</>;
   }
 }
