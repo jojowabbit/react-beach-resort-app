35365c3e3b780370e902809aed3013363e6c319a
jojowabbit <jojowabbit@gmail.com>
2020-08-27 09:22:39 +0800

    router setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
@@ -9,10 +10,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <ErrorPage />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={ErrorPage} />
+      </Switch>
     </>
   );
 }
