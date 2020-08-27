b78c26140cb9a3d488f6373b6811c51643c73cc1
jojowabbit <jojowabbit@gmail.com>
2020-08-27 09:43:37 +0800

    Navbar components setup

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import ErrorPage from "./pages/ErrorPage";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
