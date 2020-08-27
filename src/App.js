621f33bbd0c7d71db7b48bb4e5605082a904ee07
jojowabbit <jojowabbit@gmail.com>
2020-08-27 09:12:20 +0800

    page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import ErrorPage from "./pages/ErrorPage";
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <ErrorPage />
+    </>
+  );
 }
 
 export default App;
