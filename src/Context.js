49c12259fb45c4d1921441cffc3fb144ba242d24
jojowabbit <jojowabbit@gmail.com>
2020-08-28 10:45:49 +0800

    Type filter

--- src/Context.js
+++ src/Context.js
@@ -53,11 +53,36 @@ class RoomProvider extends Component {
     return room;
   };
 
-  handleChange = (e) => {
-    const type = e.target.type;
-    const name = e.target.name;
-    const value = e.target.value;
-    console.log(type, name, value);
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type == "checkbox" ? target.checked : target.value;
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets,
+    } = this.state;
+    let tempRooms = [...rooms];
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    });
   };
 
   render() {
@@ -88,3 +113,8 @@ export function withRoomConsumer(Component) {
 }
 
 export { RoomContext, RoomProvider, RoomConsumer };
+
+// 3 ways to use context
+// 1. render props {value => {}}
+// 2. HOC
+// 3. useContext hook
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 
-export default function RoomFilter() {
-  return <div>RoomFilter</div>;
+import { RoomContext } from "../Context";
+import Title from "./Title";
+
+// get unique value
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+export default function RoomFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+  // get unique types
+  let types = getUnique(rooms, "type");
+  // add all into options
+  types = ["all", ...types];
+  // map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+      </form>
+    </section>
+  );
 }
